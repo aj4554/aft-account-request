d2387c02dd4575999290d539f59162fa405f2484
Ashvin Jacinth <ashvin.jacinth@trustpower.com>
2026-02-02 09:17:31 +0000

    AFT: vend CoreInfra Network account

---
+++ terraform/network-account.tf
@@ -0,0 +1,32 @@
+module "coreinfra_network" {
+
+  source = "git::https://github.com/aws-ia/terraform-aws-control_tower_account_factory.git//modules/aft-account-request?ref=main"
+
+  account_customizations_name = "network"
+
+  control_tower_parameters = {
+    AccountEmail              = "ashvin.ajacs+coreinfra-network@gmail.com"
+    AccountName               = "network"
+    ManagedOrganizationalUnit = "Network (ou-bllk-2w14yjyh)"
+
+    SSOUserEmail     = "ashvin.jacinth@gmail.com"
+    SSOUserFirstName = "Ashvin"
+    SSOUserLastName  = "Jacinth"
+  }
+
+  account_tags = {
+    owner      = "platform"
+    foundation = "network"
+    managed_by = "aft"
+    purpose    = "network-hub"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AJ"
+    change_reason       = "Vend foundational Network account via AFT"
+  }
+
+  custom_fields = {
+    account_purpose = "network-hub"
+  }
+}
